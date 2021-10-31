b4a5a62efb13ce6b5bc7bc10bf40a432df7a52a8
Didier Pérez <mail@didierperez.com>
2021-10-30 21:20:11 -0500

    Load HomeController#index by default

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -9,10 +9,14 @@ module Rulers
         return [404, {'Content-Type' => 'text/html'}, []]
       end
 
-      klass, act = get_controller_and_action(env)
-      controller = klass.new(env)
-      text = controller.send(act)
-      [200, {'Content-Type' => 'text/html'}, [text]]
+      begin
+        klass, act = get_controller_and_action(env)
+        controller = klass.new(env)
+        text = controller.send(act)
+        return [200, {'Content-Type' => 'text/html'}, [text]]
+      rescue NameError, NoMethodError => error
+        return [200, {'Content-Type' => 'text/html'}, ['404 - Page not found']]
+      end
     end
   end
 
--- lib/rulers/routing.rb
+++ lib/rulers/routing.rb
@@ -1,6 +1,12 @@
 module Rulers
   class Application
     def get_controller_and_action(env)
+      if env['PATH_INFO'] == '/'
+        cont = "HomeController"
+        action = "index"
+        return [Object.const_get(cont), action]
+      end
+
       _, cont, action, after = env["PATH_INFO"].split('/', 4)
       cont = cont.capitalize # "People"
       cont += "Controller" # "PeopleController"
